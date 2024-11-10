4b2b4fb76ba83941e6377b07a36c911363347a94
qamberali <qamberali4904@gmail.com>
2024-11-10 16:23:34 +0500

    prompt(operators)

---
+++ script.js
@@ -0,0 +1,19 @@
+let num1 = parseFloat(prompt("Enter the first number:"));
+let num2 = parseFloat(prompt("Enter the second number:"));
+let operator = prompt("Enter an operator (+, -, *, /):");
+
+let result;
+if (operator === "+") {
+    result = num1 + num2;
+} else if (operator === "-") {
+    result = num1 - num2;
+} else if (operator === "*") {
+    result = num1 * num2;
+} else if (operator === "/") {
+        result = num1 / num2;
+    } 
+else {
+    result = "Invalid operator.";
+}
+
+alert("Result: " + result);
